ab4850ad9be406a854497cd1a058300b01752988
abhik_setia <abhiksetia@gmail.com>
2016-04-08 15:56:46 +0530

    files updated

---
+++ package.php
@@ -0,0 +1,151 @@
+<?php 
+echo '<div class="row">
+
+			<div class="row">
+				<div class="container-fluid" style="margin: 0 1%">
+
+					<div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+						<div class="container-fluid cards" id="card1" style="margin: 0;padding: 0">
+							<div class=" container top_sec" style="border-bottom: 5px solid green; " >
+								<h3>Children</h3>			
+							</div>
+							<div class="mycontainer-fluid mid_sec">
+
+									<div class="mycontainer">
+										<img src="icons/pack.png" alt="">
+									</div>
+
+									<div class="container-fluid">
+									<div class="col-lg-7 col-md-7 col-sm-7 col-xs-7" style="margin: 0 0">
+										<h3 style="font-size:1.2em ">Educate a child</h3>
+										<br>
+										<h6 style="margin-top:-15%;color: #FF7326">By: Sikhsha Organisation</h6>
+
+										</div>
+										<div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+											<img src="icons/rs.png" alt="" style="float: right; width:100%;height:100% ; margin-top:10%; ">
+										</div>
+										
+									</div>
+									<div class="container-fluid">
+										<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at the layout.</p>
+									</div>
+									<div class="container-fluid" style="margin: 0;padding: 0">
+										<button type="button" class="btn btn-default donate_btn" style="width: 100%" ><center>83 DONATIONS</center></button>
+									</div>
+							</div>
+							<hr>
+									
+						</div>			
+					</div>
+
+	<div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+						<div class="container-fluid cards" id="card1" style="margin: 0;padding: 0">
+							<div class=" container top_sec" style="border-bottom: 5px solid red; " >
+								<h3>Women</h3>			
+							</div>
+							<div class="mycontainer-fluid mid_sec">
+
+									<div class="mycontainer">
+										<img src="icons/pack.png" alt="">
+									</div>
+
+									<div class="container-fluid">
+									<div class="col-lg-7 col-md-7 col-sm-7 col-xs-7" style="margin: 0 0">
+										<h3 style="font-size:1.2em ">Educate a child</h3>
+										<br>
+										<h6 style="margin-top:-15%;color: #FF7326">By: Sikhsha Organisation</h6>
+
+										</div>
+										<div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+											<img src="icons/rs.png" alt="" style="float: right; width:100%;height:100% ; margin-top:10%; ">
+										</div>
+										
+									</div>
+									<div class="container-fluid">
+										<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at the layout.</p>
+									</div>
+									<div class="container-fluid" style="margin: 0;padding: 0">
+										<button type="button" class="btn btn-default donate_btn" style="width: 100%"><center>83 DONATIONS</center></button>
+									</div>
+							</div>
+							<hr>
+									
+						</div>			
+					</div>
+
+	<div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+						<div class="container-fluid cards" id="card1" style="margin: 0;padding: 0">
+							<div class=" container-fluid top_sec" style="border-bottom: 5px solid lightblue; " >
+								<h3>Animal</h3>			
+							</div>
+							<div class="mycontainer-fluid mid_sec">
+
+									<div class="mycontainer">
+										<img src="icons/pack.png" alt="">
+									</div>
+
+									<div class="container-fluid">
+									<div class="col-lg-7 col-md-7 col-sm-7 col-xs-7" style="margin: 0 0">
+										<h3 style="font-size:1.2em ">Educate a child</h3>
+										<br>
+										<h6 style="margin-top:-15%;color: #FF7326">By: Sikhsha Organisation</h6>
+
+										</div>
+										<div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+											<img src="icons/rs.png" alt="" style="float: right; width:100%;height:100% ; margin-top:10%; ">
+										</div>
+										
+									</div>
+									<div class="container-fluid">
+										<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at the layout.</p>
+									</div>
+									<div class="container-fluid" style="margin: 0;padding: 0">
+										<button type="button" class="btn btn-default donate_btn" style="width: 100%"><center>83 DONATIONS</center></button>
+									</div>
+							</div>
+							<hr>
+									
+						</div>			
+					</div>
+
+	<div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+						<div class="container-fluid cards" id="card1" style="margin: 0;padding: 0">
+							<div class=" container top_sec" style="border-bottom: 5px solid yellow; " >
+								<h3>Senior citizens</h3>			
+							</div>
+							<div class="mycontainer-fluid mid_sec">
+
+									<div class="mycontainer">
+										<img src="icons/pack.png" alt="">
+									</div>
+
+									<div class="container-fluid">
+									<div class="col-lg-7 col-md-7 col-sm-7 col-xs-7" style="margin: 0 0">
+										<h3 style="font-size:1.2em ">Educate a child</h3>
+										<br>
+										<h6 style="margin-top:-15%;color: #FF7326">By: Sikhsha Organisation</h6>
+
+										</div>
+										<div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+											<img src="icons/rs.png" alt="" style="float: right; width:100%;height:100% ; margin-top:10%; ">
+										</div>
+										
+									</div>
+									<div class="container-fluid">
+										<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at the layout.</p>
+									</div>
+									<div class="container-fluid" style="margin: 0;padding: 0">
+										<button type="button" class="btn btn-default  donate_btn" style="width: 100%"><center>83 DONATIONS</center></button>
+									</div>
+							</div>
+							<hr>
+									
+						</div>			
+					</div>
+
+				</div>			
+			</div>
+
+		</div>	';
+ ?>
